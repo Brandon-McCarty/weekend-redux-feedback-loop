b14e886505e0d76812fa4c706f0b18214f45c3bc
Brandon McCarty <brandon.m.mccarty12@gmail.com>
2022-04-01 15:01:46 -0500

    add redux setup in index

--- src/index.js
+++ src/index.js
@@ -4,7 +4,24 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
+// REDUX Imports
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
+// REDUCERS
+
+// STORE
+const store = createStore(
+    combineReducers({
+
+    }),
+    applyMiddleware(logger)
+)
+
 ReactDOM.render(
-<App />, 
+<Provider store={store}>
+<App />
+</Provider>, 
 document.getElementById('root'));
 registerServiceWorker();
