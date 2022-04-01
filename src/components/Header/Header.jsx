b0ccc93b1c7c9b5e1e62c13e7bc5f0b748126dcb
Brandon McCarty <brandon.m.mccarty12@gmail.com>
2022-04-01 14:10:00 -0500

    add necessary components

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+
+    return (
+        <>
+            <header className='App-header'>
+                <h1 className='App-title'>Feedback!</h1>
+                <h4>Please provide your feedback below.</h4>
+            </header>
+        </>
+    )
+}
+
+export default Header;
