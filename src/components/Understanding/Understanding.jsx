81e1a35d5289709ba98bf190fa93fdba0b2fa13a
Brandon McCarty <brandon.m.mccarty12@gmail.com>
2022-04-02 13:31:49 -0500

    refactor inputs

--- src/components/Understanding/Understanding.jsx
+++ src/components/Understanding/Understanding.jsx
@@ -1,20 +1,13 @@
-import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
-function Understanding() {
+import FeedbackInput from '../FeedbackInput/FeedbackInput';
+
+function Understanding({ handleChange, rating }) {
 
     const history = useHistory();
     const dispatch = useDispatch();
-    const [rating, setRating] = useState('');
 
-    // Set the rating as the selected radio input value
-    const handleChange = (event) => {
-        const target = event.target;
-        if (target.checked) {
-            setRating(target.value);
-        }
-    };
 
     // Dispatch the rating to feedback reducer upon submission
     const submitUnderstanding = (event) => {
@@ -29,60 +22,10 @@ function Understanding() {
             <h3>How well do you understand the material?</h3>
 
             <form onSubmit={submitUnderstanding}>
-                <input
-                    required
-                    type="radio"
-                    name="rating"
-                    id="optionOne"
-                    value="1"
-                    checked={rating == '1'}
-                    onChange={handleChange}
+                <FeedbackInput
+                    handleChange={handleChange}
+                    rating={rating}
                 />
-                <label htmlFor="optionOne">1</label>
-
-                <input
-                    required
-                    type="radio"
-                    name="rating"
-                    id="optionTwo"
-                    value="2"
-                    checked={rating == '2'}
-                    onChange={handleChange}
-                />
-                <label htmlFor="optionTwo">2</label>
-
-                <input
-                    required
-                    type="radio"
-                    name="rating"
-                    id="optionThree"
-                    value="3"
-                    checked={rating == '3'}
-                    onChange={handleChange}
-                />
-                <label htmlFor="optionThree">3</label>
-
-                <input
-                    required
-                    type="radio"
-                    name="rating"
-                    id="optionFour"
-                    value="4"
-                    checked={rating == '4'}
-                    onChange={handleChange}
-                />
-                <label htmlFor="optionFour">4</label>
-
-                <input
-                    required
-                    type="radio"
-                    name="rating"
-                    id="optionFive"
-                    value="5"
-                    checked={rating == '5'}
-                    onChange={handleChange}
-                />
-                <label htmlFor="optionFive">5</label>
                 <button type="submit">Next</button>
             </form>
         </>
